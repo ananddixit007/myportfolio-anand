6e12e702a6bdab22b342d5d72c86d6866715ec20
Anand Dixit <52794800+ananddixit007@users.noreply.github.com>
2023-05-08 12:32:55 +0530

    Update tiltAnimation.js
    Minifying

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,6 +1,1 @@
-import VanillaTilt from "vanilla-tilt";
-
-export default function initTiltAnimation() {
-  const elements = document.querySelectorAll(".js-tilt");
-  VanillaTilt.init(elements);
-}
+import VanillaTilt from "vanilla-tilt";export default function initTiltAnimation(){const elements=document.querySelectorAll(".js-tilt");VanillaTilt.init(elements)}
